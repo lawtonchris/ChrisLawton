7c700c750cdb66b7cca164c63e086dabce314d3f
Chris Lawton <lawton.c@gmail.com>
2025-07-10 15:18:28 -0400

    fix for npm start

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http')
 const fs = require('fs')
+const express = require("express");
+
+const app = express();
+
+
+module.exports = app;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
+  fs.createReadStream('views/index.html').pipe(res)
 })
 
-server.listen(process.env.PORT || 3000)
+//server.listen(process.env.PORT || 3000)
